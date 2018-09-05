355403e0db977b011dd6493c9cadb98d7aba42c7
OskarGrzelak <oskargrzelak@gmail.com>
2018-09-05 14:19:05 +0200

    game model and game view after code refactoring (so far)

--- js/script.js
+++ js/script.js
@@ -1,67 +1,50 @@
-const message = document.querySelector('.header__message');
-const timer = document.querySelector('.header__timer span');
-const squares = document.querySelector('.squares');
-const start = document.querySelector('.btn--start');
-
-console.log(timer.innerHTML);
-
-class Game {
+class GameModel {
     constructor() {
         this.isPlayed = false;
         this.colors = [];
-        this.comparedColors = [];
+        this.comparedElements = [];
     }
 
-    setSquaresAmount(squaresAmount) {
-        this.squaresAmount = squaresAmount;
+    setSquaresAmount(squaresAmount) { this.squaresAmount = squaresAmount; }
+
+    setGameMode(gameMode) { this.gameMode = gameMode; }
+
+    setTimeMode(timeMode) { this.timeMode = timeMode; }
+
+    setCounter() { this.counter = this.squaresAmount / 2; }
+
+    resetTimer() { this.timer = 0; }
+
+    updateTimer() { this.timer ++; }
+
+    pickColor() {
+        return `rgb(${Math.floor(Math.random()*16) * 16}, ${Math.floor(Math.random()*16) * 16}, ${Math.floor(Math.random()*16) * 16})`;
     }
 
-    setGameMode(gameMode) {
-        this.gameMode = gameMode;
-    }
-
-    setTimeMode(timeMode) {
-        this.timeMode = timeMode;
-    }
-
-    setCounter() {
-        this.counter = this.squaresAmount / 2;
-    }
-
-    resetTimer() {
-        this.timer = 0;
-    }
-
-    runTime() {
-        let intervalID = setInterval(() => {
-            game.timer = game.timer + 1;
-            if (!game.isPlayed) clearInterval(intervalID);
-        }, 1000);
-    }
-
-    pickColors(num) {
+    setColors(num) {
         for (let i = 0; i < num/2; i ++) {
-            const red = Math.floor(Math.random()*16) * 16;
-            const green = Math.floor(Math.random()*16) * 16;
-            const blue = Math.floor(Math.random()*16) * 16;
-            this.colors.push(`rgb(${red}, ${green}, ${blue})`);
-            this.colors.push(`rgb(${red}, ${green}, ${blue})`);
-        }
-    }
-
-    setColors(squares, colors) {
-        squares.forEach(el => {
-            const index = Math.floor(Math.random()*(colors.length - 1));
-            el.style.backgroundColor = colors[index];
-            colors.splice(index, 1);
-        });
+            this.colors.push(this.pickColor());
+            this.colors.push(this.colors[i*2]);
+        };
     }
 }
 
-const gameView = {
-    getSquaresAmount() { return 6; },
-    getGameMode() { return 'colors' },
-    getTimeMode() { return 'normal'},
+class GameView {
+    constructor() {
+        this.elements = {
+            message: document.querySelector('.header__message'),
+            timer: document.querySelector('.header__timer span'),
+            squares: document.querySelector('.squares'),
+            start: document.querySelector('.btn--start'),
+        }
+    }
+
+    getSquaresAmount() { return 6; }
+
+    getGameMode() { return 'colors'; }
+
+    getTimeMode() { return 'normal'; }
+
     renderSquare(id) {
         const markup = `
             <div class="square" id="square-${id}" >
@@ -70,86 +53,97 @@ const gameView = {
             </div>
         `;
         document.querySelector('.squares').insertAdjacentHTML('beforeend', markup);
-    },
-    renderSquares(num) {
-        for (let i = 0; i < num; i ++) {
-            this.renderSquare(i+1);
-        }
-    },
-    clearSquares() {
-        document.querySelector('.squares').innerHTML = '';
-    },
-    displayMessage(type) {
-        type === 'win' ? message.innerHTML = 'Congrats. You won' : message.innerHTML = 'You lost. Try again';
-    },
-    clearMessage() {
-        message.innerHTML = '';
-    },
-    displayTimer() {
-        let intervalID = setInterval(() => {
-            timer.innerHTML = game.timer;
-            console.log(game.isPlayed);
-            if (!game.isPlayed) clearInterval(intervalID);
-        }, 1000);
-    },
-    clearTimer() {
-        timer.innerHTML = 0;
+        return document.querySelector('.squares').lastElementChild;
     }
+
+    colorSquare(square, colors) {
+        const index = Math.floor(Math.random()*(colors.length - 1));
+        square.children[1].style.backgroundColor = colors[index];
+        colors.splice(index, 1);
+    }
+
+    renderSquares(num, colors) {
+        for (let i = 0; i < num; i ++) {
+            const square = this.renderSquare(i+1);
+            this.colorSquare(square, colors)
+        };
+    }
+
+    clearSquares() { document.querySelector('.squares').innerHTML = ''; }
+    
+    displayMessage(type) {
+        type === 'win' ? this.elements.message.innerHTML = 'Congrats. You won' : this.elements.message.innerHTML = 'You lost. Try again';
+    }
+
+    clearMessage() { this.elements.message.innerHTML = ''; }
+
+    displayTimer(time) { this.elements.timer.innerHTML = time; }
+
+    clearTimer() { this.elements.timer.innerHTML = 0; }
 };
 
-const game = new Game();
+const gameModel = new GameModel();
+const gameView = new GameView();
 
 // Starting the game
 
-start.addEventListener('click', e => {
+const runTime = () => {
+    let intervalID = setInterval(() => {
+        gameModel.updateTimer();
+        gameView.displayTimer(gameModel.timer);
+        if (!gameModel.isPlayed) clearInterval(intervalID);
+    }, 1000);
+}
+
+gameView.elements.start.addEventListener('click', e => {
     gameView.clearSquares();
     gameView.clearMessage();
     gameView.clearTimer();
-    game.resetTimer();
-    game.setSquaresAmount(gameView.getSquaresAmount());
-    game.pickColors(game.squaresAmount);
-    gameView.renderSquares(game.squaresAmount);
+    gameModel.resetTimer();
+    gameModel.setSquaresAmount(gameView.getSquaresAmount());
+    gameModel.setColors(gameModel.squaresAmount);
+    gameView.renderSquares(gameModel.squaresAmount, gameModel.colors);
     const squaresSide = document.querySelectorAll('.square__side--back');
-    game.setColors(Array.from(squaresSide), game.colors);
-    game.setCounter();
-    game.isPlayed = true;
-    game.runTime();
-    gameView.displayTimer();
+    gameModel.setColors(Array.from(squaresSide), gameModel.colors);
+    gameModel.setCounter();
+    gameModel.isPlayed = true;
+    runTime();
 });
 
 // Handling clicking on squares
 
-squares.addEventListener('click', e => {
-    if(game.isPlayed) {
+gameView.elements.squares.addEventListener('click', e => {
+    if(gameModel.isPlayed) {
         const square = e.target.closest('.square');
-        if (game.comparedColors.length < 2 && !square.classList.contains('active')) {
-            square.classList.add('active');
-            square.children[0].style.transform = 'rotateY(180deg)';
-            square.children[1].style.transform = 'rotateY(0)';
-            game.comparedColors.push(square);
-        }
-        setTimeout(()=> {
-            if (game.comparedColors.length === 2) {
-                if (game.comparedColors[0].children[1].style.backgroundColor === game.comparedColors[1].children[1].style.backgroundColor) {
-                    game.comparedColors[0].style.visibility = 'hidden';
-                    game.comparedColors[1].style.visibility = 'hidden';
-                    game.counter --;
-                } else {
-                    game.comparedColors[0].children[0].style.transform = 'rotateY(0)';
-                    game.comparedColors[0].children[1].style.transform = 'rotateY(180deg)';
-                    game.comparedColors[0].classList.remove('active');
-                    game.comparedColors[1].children[0].style.transform = 'rotateY(0)';
-                    game.comparedColors[1].children[1].style.transform = 'rotateY(180deg)';
-                    game.comparedColors[1].classList.remove('active');
-                }
-                game.comparedColors.splice(0, 2);
-                if (game.counter === 0) {
-                    game.isPlayed = false;
-                    gameView.displayMessage('win');
-                }
+        if(square) {
+            if (gameModel.comparedElements.length < 2 && !square.classList.contains('active')) {
+                square.classList.add('active');
+                square.children[0].style.transform = 'rotateY(180deg)';
+                square.children[1].style.transform = 'rotateY(0)';
+                gameModel.comparedElements.push(square);
             }
-        }, 1000);
-
+            setTimeout(()=> {
+                if (gameModel.comparedElements.length === 2) {
+                    if (gameModel.comparedElements[0].children[1].style.backgroundColor === gameModel.comparedElements[1].children[1].style.backgroundColor) {
+                        gameModel.comparedElements[0].style.visibility = 'hidden';
+                        gameModel.comparedElements[1].style.visibility = 'hidden';
+                        gameModel.counter --;
+                    } else {
+                        gameModel.comparedElements[0].children[0].style.transform = 'rotateY(0)';
+                        gameModel.comparedElements[0].children[1].style.transform = 'rotateY(180deg)';
+                        gameModel.comparedElements[0].classList.remove('active');
+                        gameModel.comparedElements[1].children[0].style.transform = 'rotateY(0)';
+                        gameModel.comparedElements[1].children[1].style.transform = 'rotateY(180deg)';
+                        gameModel.comparedElements[1].classList.remove('active');
+                    }
+                    gameModel.comparedElements.splice(0, 2);
+                    if (gameModel.counter === 0) {
+                        gameModel.isPlayed = false;
+                        gameView.displayMessage('win');
+                    }
+                }
+            }, 1000);
+        }
         
     }
 });
